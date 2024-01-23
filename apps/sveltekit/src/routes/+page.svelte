851ab7ff58b05c6130ecbefc545e607899f920da
Jedliu <jedliu@gmail.com>
2024-01-23 20:39:41 +0800

    add sveltekit app

---
+++ apps/sveltekit/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { meaningOfLife } from '@nighttrax/foo';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<div>Meaning of life: {meaningOfLife}</div>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
